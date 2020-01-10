22a2109e10d9a810111b8190f5b2fdcb20587ec9
Dylan Premo <maboose@yahoo.com>
2020-01-10 15:50:59 -0800

    Commit after tutorial #10

--- src/components/layout/SignedOutLinks.js
+++ src/components/layout/SignedOutLinks.js
@@ -1,14 +1,14 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const SignedOutLinks = () => {
   return (
-    <ul className="right">
+    <ul className='right'>
       <li>
-        <NavLink to="/">Signup</NavLink>
+        <NavLink to='/signup'>Signup</NavLink>
       </li>
       <li>
-        <NavLink to="/">Login</NavLink>
+        <NavLink to='/signin'>Login</NavLink>
       </li>
     </ul>
   );
